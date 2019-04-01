48595c795b236ba9d6e2f7cabe149204bb45620a
Luca Ferroni <luca@befair.it>
2019-04-01 16:50:57 +0200

    Ora viene gestita e visualizzata una lista di stringhe

--- stampalista.c
+++ stampalista.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
     // 0. Definizione di sala_di_attesa che contiene elementi di tipo char[]
-    // char[] paziente;
-    int paziente;
-    int sala_di_attesa[10];
+    char paziente[20];
+    char sala_di_attesa[10][20];
     int ne;
     int i;
     // 1. Richiesta di quanti pazienti sono presenti
@@ -18,16 +18,16 @@ int main(void) {
 
     // 2. Richiesta nomi in input e inserimento nella sala_di_attesa
     for (i = 0; i<ne; i++) {
-        printf("Numero: ");
-        scanf("%i", &paziente);
-        sala_di_attesa[i] = paziente;
+        printf("Nome: ");
+        scanf("%s", paziente);
+        strcpy(sala_di_attesa[i], paziente);
     }
 
     // 3. Fare un ciclo su ogni elemento della sala di attesa:
     //  3a. Stampare il nome contenuto
     printf("I pazienti sono: ");
     for (i = 0; i<ne; i++) {
-        printf("%i ", sala_di_attesa[i]);
+        printf("%s ", sala_di_attesa[i]);
     }
     printf("\n");
 }
