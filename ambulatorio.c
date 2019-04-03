ce7ae2bca3051ed6e0716100ee0e787999f72b2a
Luca Ferroni <luca@befair.it>
2019-04-03 17:57:43 +0200

    Suddivise le funzioni del programma ambulatorio

--- ambulatorio.c
+++ ambulatorio.c
@@ -2,34 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+int get_num_pazienti(int max_pazienti);
+void get_sala_di_attesa(int ne, char sala_di_attesa[10][20]);
+void uscita_pazienti(int ne, char sala_di_attesa[10][20]);
+
 int main(void) {
 
     // 0. Definizione di sala_di_attesa che contiene elementi di tipo char[]
-    char paziente[20];
     char sala_di_attesa[10][20];
     int ne;
-    int i;
-    char tasto;
 
     // 1. Richiesta di quanti pazienti sono presenti
-    do {
-        printf("Quanti pazienti sono presenti [da 1 a 10]? ");
-        scanf("%i", &ne);
-    } while (ne > 10);
-
+    ne = get_num_pazienti(10);
     printf("Bene, %i pazienti presenti\n", ne);
 
     // 2. Richiesta nomi in input e inserimento nella sala_di_attesa
+    get_sala_di_attesa(ne, sala_di_attesa);
+
+    // 3. Lettura del carattere premuto -> 
+    //      se è '*' => toglie il paziente che è arrivato per primo e aspetta 2 secondi
+    //      altrimenti non fa nulla.
+    uscita_pazienti(ne, sala_di_attesa);
+}
+
+int get_num_pazienti(int max_pazienti) {
+    // Restituisce il numero di pazienti inserito dall'utente
+    int ne;
+    do {
+        printf("Quanti pazienti sono presenti [da 1 a %i]? ", max_pazienti);
+        scanf("%i", &ne);
+    } while (ne > max_pazienti);
+    return ne;
+}
+
+void get_sala_di_attesa(int ne, char sala_di_attesa[10][20]) {
+    int i;
+    char paziente[20]; 
+
     for (i = 0; i<ne; i++) {
         printf("Nome: ");
         scanf("%s", paziente);
         strcpy(sala_di_attesa[i], paziente);
     }
+}
 
-    // 3. Lettura del carattere premuto -> 
-    //      se è '*' => toglie il paziente che è arrivato per primo e aspetta 2 secondi
-    //      altrimenti non fa nulla.
-    i = 0;
+void uscita_pazienti(int ne, char sala_di_attesa[10][20]) {
+    int i = 0;
+    char tasto;
+
     while (i<ne) {
         tasto = getchar();
         if (tasto == '*') {
@@ -38,3 +58,4 @@ int main(void) {
         }
     }
 }
+
