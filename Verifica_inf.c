9b3f93621566b311c0623684e0bd194b9e788940
Frax345 <50922885+Frax345@users.noreply.github.com>
2019-05-22 18:42:08 +0200

    Add files via upload

--- Verifica_inf.c
+++ Verifica_inf.c
@@ -12,6 +12,9 @@ typedef struct
 
 }Svoto;
 
+int carica(char Verifica_inf[],Svoto tab[],int *ne);
+int Salva(char Verifica_inf,Svoto tab[],int ne);
+int insert_voto(Svoto tab[],int* ne,Svoto x);
 
 
 
@@ -23,8 +26,54 @@ void main(void)
     int pos;
     char ch;
 
+    Carica("voti.bin",tab,&ne);
+
+    do
+    {
+        MostraMenu();
+        ch = _getch();
+        switch(ch)
+        {
+            
+            case '1' :
+            system("cls");
+            printf("materia:");
+            scanf("%s",x.mat);
+            fflush(stdin);
+            printf("Voto: ");
+            scanf("%f",&x.voto);
+            fflush(stdin);
+            insert_voto(tab,&ne,x);
+            Salva("voti.bin",tab,ne);
+            break;
+          
+            case '2':
+            Visualizza(tab,ne);
+            system("pause");
+            break;
+            
+            case '3':
+            Visualizza(tab,ne);
+            printf("N.riga da cancellare:");
+            scanf("%i", &pos);
+            break;
+                    
+            }
+        
+    } while (ch != '*');
+    system("pause");
+    return 0;
+    
+
 
 }
+
+
+
+
+
+
+
 
 void MostraMenu(void){
     system("cls");
@@ -38,6 +87,24 @@ void MostraMenu(void){
 
 }
 
+int insert_voto(Svoto tab[],int* ne,Svoto x)
+{
+    if(*ne >= DIM)
+    return 0;
+    tab[*ne]=x;
+    *ne = *ne+1;
+    return 1;
+}
+
+void Visualizza(Svoto tab[],int ne)
+{
+    int i;
+    system("cls");
+    printf("Lista dei voti\n");
+    printf("materia tipo voto\n");
+    for(i=0;i<ne;i++)
+    printf("%2i %-6s %-4c  %.2f\n",i,tab[i].mat,tab[i].tipo,tab[i].voto);
+}
 
 
 
@@ -56,4 +123,18 @@ int carica(char Verifica_inf[],Svoto tab[],int *ne)
     fread(&tab[i],sizeof(Svoto),i,f);
     fclose(f);
     return 1;
+}
+int Salva(char Verifica_inf,Svoto tab[],int ne)
+{
+    int i;
+    FILE* f;
+    f = fopen(Verifica_inf,"wb");
+    if ( f== 0)
+    return 0;
+    fwrite(&ne,sizeof(int),1,f);
+    for(i=0;i<ne;i++)
+    fwrite(&tab[i],sizeof(Svoto),1,f);
+    fclose(f);
+    return 1;
+
 }
